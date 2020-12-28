a9c3d216aab43296a1767ccdbfe89ee8d73215c2
amiru11 <wrfg12@gmail.com>
2020-12-28 12:48:36 +0900

    Fix: Error for path

--- src/server/serverRender.tsx
+++ src/server/serverRender.tsx
@@ -5,7 +5,7 @@ import ReactDOMServer from 'react-dom/server';
 import { StaticRouter } from 'react-router-dom';
 import { ChunkExtractor } from '@loadable/server';
 import { FilledContext, HelmetProvider } from 'react-helmet-async';
-import App from '../App';
+import App from '../shared/App';
 import Html from './Html';
 
 /**
